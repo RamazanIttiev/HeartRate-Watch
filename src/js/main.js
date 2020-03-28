36b265afc66ebacc2978bd896c9d7fe82b6751f8
Ramazan <vip.ittiev@mail.ru>
2020-03-28 08:59:42 +0100

    marcked up tabs and item cards, without adaptation

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,12 @@
-$(document).ready(function () {
-  $('.slider__inner').slick({
-    speed: 1200,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.svg"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.svg"></button>',
-    responsive: [{
-      breakpoint: 992,
-      settings: {
-        dots: true,
-        arrows: false
-      }
-    }]
-  });
+
+
+const slider = tns({
+  container: '.slider__inner',
+  items: 1,
+  slideBy: 'page',
+  controlsText: [
+    '<img src="icons/arrow_left.svg">',
+    '<img src="icons/arrow_right.svg">'
+  ],
+  nav: false
 });
