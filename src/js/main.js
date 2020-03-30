452d1032aa1a0d613e2757b8d377c214bccb7044
Ramazan <vip.ittiev@mail.ru>
2020-03-30 20:33:26 +0300

    Adapted and finished cards and tabs, with JS

--- src/js/main.js
+++ src/js/main.js
@@ -4,18 +4,31 @@ const slider = tns({
   container: '.slider__inner',
   items: 1,
   slideBy: 'page',
-  controlsText: [
-    '<img src="icons/arrow_left.svg">',
-    '<img src="icons/arrow_right.svg">'
-  ],
+  // controlsText: [
+  //   '<img src="icons/arrow_left.svg">',
+  //   '<img src="icons/arrow_right.svg">'
+  // ],
   nav: false,
   responsive: {
+    992: {
+      nav: false,
+      controls: false
+    },
     320: {
-      controls: false,
-
+      nav: true,
+      controls: false
     }
   }
 });
+
+document.querySelector('.prev').onclick = function () {
+  slider.goTo('prev');
+};
+
+document.querySelector('.next').onclick = function () {
+  slider.goTo('next');
+};
+
 
 // !!!!!!!!!!!!! TABS !!!!!!!!!!!!!!!
 
