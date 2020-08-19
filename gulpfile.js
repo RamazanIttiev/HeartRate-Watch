3de2e68c619001e7ffee26cae4dd5e89bf3ec8f3
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2020-08-19 12:15:45 +0300

    Optimized

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,51 @@
-const gulp = require("gulp");
-const browserSync = require("browser-sync");
-const sass = require("gulp-sass");
-const cleanCSS = require("gulp-clean-css");
-const autoprefixer = require("gulp-autoprefixer");
-const rename = require("gulp-rename");
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const ghPages = require('gulp-gh-pages');
 
-gulp.task("server", function() {
+gulp.task('server', function () {
   browserSync({
     server: {
-      baseDir: "src"
-    }
+      baseDir: 'src',
+    },
   });
 
-  gulp.watch("src/*.html").on("change", browserSync.reload);
+  gulp.watch('src/*.html').on('change', browserSync.reload);
 });
 
-gulp.task("styles", function() {
+gulp.task('styles', function () {
   return gulp
-    .src("src/sass/**/*.+(scss|sass)")
+    .src('src/sass/**/*.+(scss|sass)')
     .pipe(
       sass({
-        outputStyle: "compressed"
-      }).on("error", sass.logError)
+        outputStyle: 'compressed',
+      }).on('error', sass.logError)
     )
     .pipe(
       rename({
-        suffix: ".min",
-        prefix: ""
+        suffix: '.min',
+        prefix: '',
       })
     )
     .pipe(autoprefixer())
     .pipe(
       cleanCSS({
-        compatibility: "ie8"
+        compatibility: 'ie8',
       })
     )
-    .pipe(gulp.dest("src/css"))
+    .pipe(gulp.dest('src/css'))
     .pipe(browserSync.stream());
 });
 
-gulp.task("watch", function() {
-  gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
+gulp.task('watch', function () {
+  gulp.watch('src/sass/**/*.+(scss|sass)', gulp.parallel('styles'));
 });
 
-gulp.task("default", gulp.parallel("watch", "server", "styles"));
+gulp.task('deploy', function () {
+  return gulp.src('./src/**/*').pipe(ghPages());
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
